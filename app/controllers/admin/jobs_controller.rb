bb3a56f2a4d8b0ad217c41897f3d419012441f76
simon <wangzd.fudan@gmail.com>
2017-01-22 23:30:34 +0800

    add is_hidden to job and DESC view and change the backend view

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -2,7 +2,7 @@ class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
 
-  # move followed def to ApplicationController
+  # /. move followed def to ApplicationController
   # def require_is_admin
   #   if !current_user.admin?
   #     flash[:alert] = "you are not admin"
@@ -57,6 +57,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
