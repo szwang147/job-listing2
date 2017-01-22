2ad848f43b0d00020832671e7de3c3b8f2b17a99
simon <wangzd.fudan@gmail.com>
2017-01-22 18:14:02 +0800

    add is_admin to user and implement the crud admin#jobs

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,62 @@
+class Admin::JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :require_is_admin
+
+  # move followed def to ApplicationController
+  # def require_is_admin
+  #   if !current_user.admin?
+  #     flash[:alert] = "you are not admin"
+  #     redirect_to jobs_path
+  #   end
+  # end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+
+    if @job.save
+      redirect_to admin_jobs_path
+      flash[:notice] = "create"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+      flash[:notice] = "update"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:notice] = "delete"
+    redirect_to admin_jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
